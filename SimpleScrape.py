d4f393d7ad14a63903ded3c872e5c9bc7af6dbd9
luke robertson <robertson.stuff@gmail.com>
2020-11-13 15:38:19 +1100

    Update SimpleScrape.py

--- SimpleScrape.py
+++ SimpleScrape.py
@@ -1,21 +1,8 @@
 
-
 import os
 
 from requests_html import HTML, HTMLSession
 from twilio.rest import Client
-
-#with open('simpleweb2.html') as html_file:
-#    source = html_file.read()
-#    html = HTML(html=source)
-#
-#print(html.text)
-#forecast = html.find('div.forecast', first=True)
-#headline = forecast.find('h2', first=True).text
-#Summary = html.find('p', first=True).text
-#
-#print(headline)
-#print(Summary)
 
 twilio_num = os.environ['TWILIO_NUMBER']
 personal_num = os.environ['MY_NUMBER']
