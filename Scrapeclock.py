4294b0719e12cbb19e66bc62b28dbd0d736d4cbc
luke robertson <robertson.stuff@gmail.com>
2020-11-13 15:40:45 +1100

    Update Scrapeclock.py

--- Scrapeclock.py
+++ Scrapeclock.py
@@ -6,7 +6,6 @@ from SimpleScrape import *
 
 sched = BlockingScheduler()
 
-# Schedule job_function to be called every blah
-#sched.add_job(send_goodtimes, 'interval', seconds=30)
+# Schedule job_function to be called every 24hrs from a certain date at 7:30am Canberra time
 sched.add_job(send_goodtimes, 'interval', hours=24, start_date='2010-10-01 20:30:00')
 sched.start()
